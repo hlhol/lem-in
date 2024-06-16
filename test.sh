0366e69e7d0f21bf0da28b24cba109ee303b2d62
hlhol <hlhols313@gmail.com>
2024-06-16 20:52:56 +0300

    add a test file

---
+++ test.sh
@@ -0,0 +1,37 @@
+go run . examples/example00.txt
+
+read -p "Press enter to continue"
+
+go run . examples/example01.txt
+
+read -p "Press enter to continue"
+
+go run . examples/example02.txt
+
+read -p "Press enter to continue"
+
+go run . examples/example03.txt
+
+read -p "Press enter to continue"
+
+go run . examples/example04.txt
+
+read -p "Press enter to continue"
+
+go run . examples/example05.txt
+
+read -p "Press enter to continue"
+
+go run . examples/badexample00.txt
+
+read -p "Press enter to continue"
+
+go run . examples/badexample01.txt
+
+read -p "Press enter to continue"
+
+go run . examples/example06.txt
+
+read -p "Press enter to continue"
+
+go run . examples/example07.txt
